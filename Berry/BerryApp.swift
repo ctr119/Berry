eee34fce05153173e856fb3444f5bda46148392e
Víctor Barrios <sygmaz19@gmail.com>
2024-02-03 00:41:20 +0100

    Initial Commit

---
+++ Berry/BerryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerryApp.swift
+//  Berry
+//
+//  Created by Víctor Barrios Sánchez on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BerryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
